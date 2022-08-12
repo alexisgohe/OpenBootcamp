da733b1e6e2c2d5be399a76226562f59adf7c021
AlexisGH <89100320+alexisgohe@users.noreply.github.com>
2022-08-11 21:30:09 -0500

    Add files via upload

---
+++ JavaScript/Ejercicio2/main.js
@@ -0,0 +1,12 @@
+const yo ={
+    nombre: "Alexis",
+    edad: 25,
+    desarrollador: true,
+    fecha: new Date(1997, 03, 17),
+    libro:{
+        titulo: "Codigo Limpio",
+        autor: "Robert C. Martin", 
+        fecha: new Date(2008, 07, 01),
+        url: "https://www.amazon.com.mx/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882/ref=asc_df_0132350882/?tag=gledskshopmx-20&linkCode=df0&hvadid=343406472608&hvpos=&hvnetw=g&hvrand=18222924718979845561&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1031201&hvtargid=pla-435472505264&psc=1"
+    }
+}
