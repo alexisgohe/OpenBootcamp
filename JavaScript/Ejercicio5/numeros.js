0b7bc3ec0a8be285215490c255469d1491dbdf3d
AlexisGH <89100320+alexisgohe@users.noreply.github.com>
2022-08-15 15:46:34 -0500

    Add files via upload

---
+++ JavaScript/Ejercicio5/numeros.js
@@ -0,0 +1,12 @@
+var altura_cm = 170;
+var altura_m = 1.70;
+var peso_kg = 71;
+
+var altura_r = Math.ceil(altura_m);
+console.log(altura_r)
+
+var peso_r = Math.floor(peso_kg);
+console.log(peso_r)
+
+var maximo =  Number.MAX_VALUE + 1 ===  Number.MAX_VALUE
+console.log(maximo)
