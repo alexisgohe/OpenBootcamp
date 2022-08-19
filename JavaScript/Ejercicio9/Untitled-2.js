6b7bfcbfd40a838fb212cf10ca2f565900b70477
AlexisGH <89100320+alexisgohe@users.noreply.github.com>
2022-08-18 20:38:54 -0500

    Add files via upload

---
+++ JavaScript/Ejercicio9/Untitled-2.js
@@ -0,0 +1,11 @@
+// - Una función sin parámetros que devuelva siempre "true"
+function Ptrue(objeto = true){
+    console.log(objeto);
+}
+
+Ptrue();
+
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+
+
+// - Una función generadora de índices pares automáticos
